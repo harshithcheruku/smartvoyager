519671db69857586ce9ef4b3e90b7bb6d85ce538
harshithcheruku <cherukuharshith4707@gmail.com>
2026-04-25 06:34:45 +0530

    modified app.py

--- app.py
+++ app.py
@@ -40,8 +40,7 @@ def create_app():
 
     return app
 
-
+app = create_app()
 # ✅ OUTSIDE the function
-if __name__ == '__main__':
-    app = create_app()
+if __name__ == '__main__':    
     app.run(host="0.0.0.0", port=5000)
